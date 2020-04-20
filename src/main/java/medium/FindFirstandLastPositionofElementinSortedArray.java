d59965a5a0251b8f3d92c89b88c02ed11ae33b6e
Satej Sawant <satejs93@gmail.com>
2020-04-20 12:33:14 +0530

    Updated calculation to prevent overflow

--- src/main/java/medium/FindFirstandLastPositionofElementinSortedArray.java
+++ src/main/java/medium/FindFirstandLastPositionofElementinSortedArray.java
@@ -22,7 +22,7 @@ public class FindFirstandLastPositionofElementinSortedArray {
         int start = 0, end = nums.length - 1;
 
         while (start <= end){
-            int mid = (start + end)/2;
+            int mid = start + (end-start)/2;
 
             if(nums[mid] == target){
                 end = mid - 1;
@@ -49,7 +49,7 @@ public class FindFirstandLastPositionofElementinSortedArray {
         int start = 0, end = nums.length - 1;
 
         while (start <= end){
-            int mid = (start + end)/2;
+            int mid = start + (end-start)/2;
 
             if(nums[mid] == target){
                 start = mid + 1;
