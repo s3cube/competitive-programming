ff29eaa2d2a1a630de34a2d887dc34352214e62b
Satej Sawant <satejs93@gmail.com>
2020-04-22 14:24:49 +0530

    Tried LC DP soln

--- src/main/java/medium/UniquePaths.java
+++ src/main/java/medium/UniquePaths.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.Arrays;
+
 public class UniquePaths {
 
 
@@ -13,9 +15,24 @@ public class UniquePaths {
 
     public int uniquePaths(int m, int n) {
 
-        traverseGridByBackTracking(0,0, m, n);
+//        traverseGridByBackTracking(0,0, m, n);
+//
+//        return globalCount;
 
-        return globalCount;
+
+        int[][] d = new int[m][n];
+
+        for(int[] arr : d) {
+            Arrays.fill(arr, 1);
+        }
+        for(int col = 1; col < m; ++col) {
+            for(int row = 1; row < n; ++row) {
+                d[col][row] = d[col - 1][row] + d[col][row - 1];
+            }
+        }
+        return d[m - 1][n - 1];
+
+
     }
 
     public void traverseGridByBackTracking(int x, int y, int m, int n){
